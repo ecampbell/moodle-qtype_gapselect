4a7ee5b19383e5146842a1751e19e68e1d8cfc31
Eoin Campbell <campbeeo@tcd.ie>
2015-12-11 17:49:34 +0000

    Identify the WordTable-compatible variant in the release label

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120200;
+$plugin->version   = 2015120800;
 $plugin->requires  = 2013101800;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_gapselect';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.7 for Moodle 2.6+';
+$plugin->release   = '1.7wt for Moodle 2.6+';
 
 $plugin->outestssufficient = true;
