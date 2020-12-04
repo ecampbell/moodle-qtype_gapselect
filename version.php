85f8a19584f084088270bce70311b76dde0ee50e
Eoin Campbell <campbeeo@tcd.ie>
2020-12-04 15:22:48 +0000

    Match standard version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061500;
+$plugin->version   = 2020061501;
 $plugin->requires  = 2020060900;
 
 $plugin->component = 'qtype_gapselect';
